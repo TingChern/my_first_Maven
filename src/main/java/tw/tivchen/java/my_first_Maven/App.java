5d0d6088a968054dbb40eb7578acb0fb1ba699a1
Ting Chen <ny88619@gmail.com>
2018-01-27 21:44:58 +0800

    my_first_Maven

---
+++ src/main/java/tw/tivchen/java/my_first_Maven/App.java
@@ -0,0 +1,13 @@
+package tw.tivchen.java.my_first_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/tw/tivchen/java/my_first_Maven/HelloMaven.java
@@ -0,0 +1,9 @@
+package tw.tivchen.java.my_first_Maven;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven!");
+	}
+
+}
